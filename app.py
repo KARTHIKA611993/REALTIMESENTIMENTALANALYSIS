ebe62e51959a73c476a6e9ad5d23d8ddc2f4e57e
KARTHIKA611993 <51583052+KARTHIKA611993@users.noreply.github.com>
2021-09-22 00:25:54 +0530

    FILES

---
+++ app.py
@@ -0,0 +1,21 @@
+
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.write("# Real Time Sentiment Analysis")
+from PIL import Image
+image = Image.open('intro.jpg')
+st.image(image, caption='Valuable comments')
+
+user_input = st.text_input("Please rate our services >>: ")
+nltk.download("vader_lexicon")
+s = SentimentIntensityAnalyzer()
+score = s.polarity_scores(user_input)
+
+
+if score == 0:
+    st.write(" ")
+elif score["neg"] != 0:
+    st.write("# Negative")
+elif score["pos"] != 0:
+    st.write("# Positive")
